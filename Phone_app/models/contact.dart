784c0130d162ffec774988bd480050a779c23cc5
RebecaGR <89923150+RebecaGR@users.noreply.github.com>
2022-01-27 19:11:33 +0100

    Add files via upload

---
+++ Phone_app/models/contact.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class EmergencyContact {
+  final String name;
+  final String phoneNumber;
+
+  EmergencyContact({
+    @required this.name,
+    @required this.phoneNumber,
+  });
+}
---
+++ Phone_app/models/user.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+class User {
+  String userName;
+  File profilePicture;
+
+  User({this.userName, this.profilePicture});
+}
